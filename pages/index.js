d085f4fe4401cc2b39ddcf1de8d5e1947a6e8261
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-01-09 16:35:40 -0500

    fix: fixed var name for form input tags

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
     e.preventDefault();
 
     try {
-      const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${cryptoName}&vs_currencies=${currency}`)
+      const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${cryptoName}&vs_currencies=${currency}`);
       const price = response.data[cryptoName][currency];
 
       setData(...data, {cryptoName: price});
@@ -39,11 +39,12 @@ export default function Home() {
       <form>
         <input type="text" name="cryptoName" placeholder="Crypto Name" value={cryptoNameInput} onChange={(e) => setCryptoNameInput(e.target.value)} />
         <input type="text" name="currency" placeholder="Currency (usd or cad)" value={currencyInput} onChange={(e) => setCurrencyInput(e.target.value)} />
-        <button type="submit" onClick={(e) => addPriceHandler(e, cryptoName, currency)}>Add Price</button>
+        <button type="submit" onClick={(e) => addPriceHandler(e, cryptoNameInput, currencyInput)}>Add Price</button>
       </form>
 
       {
         data.map((cryptoObject, index) => {
+          console.log(index);
           return (
             <div key={index}>
               <h1>{cryptoObject.cryptoName}</h1>
