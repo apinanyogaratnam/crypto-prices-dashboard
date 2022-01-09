0f3163ac40145927d2b8c88ef29da576f2592f9e
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-01-09 16:24:19 -0500

    refactor: created add price handler

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {return undefined;});
+
+  const addPriceHandler = async (e, cryptoName, currency) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${cryptoName}&vs_currencies=${currency}`)
+      const price = response.data[cryptoName][currency];
+
+      setData(...data, {cryptoName: price});
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
